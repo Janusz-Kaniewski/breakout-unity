2c1d55b818727e234d0f3b1c2f3e5a0ec6e25932
unknown <janusz.kaniewski@outlook.com>
2026-03-22 13:50:59 +0100

    Added random color to block

--- Assets/BlockScript.cs
+++ Assets/BlockScript.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class BlockScript : MonoBehaviour
 {
     private SceneScript sceneScript;
+    private SpriteRenderer spriteRenderer;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sceneScript = GameObject.FindGameObjectWithTag("SceneScript").GetComponent<SceneScript>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteRenderer.color = new Color(Random.Range(0, 255) / 255.0f, Random.Range(0, 255) / 255.0f, Random.Range(0, 255) / 255.0f);
     }
 
     // Update is called once per frame
